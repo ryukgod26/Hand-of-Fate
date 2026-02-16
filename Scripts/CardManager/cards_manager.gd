c068ddeff7bcecc4d2c1af22c7fe797440e49f83
raviwhitehat <ekravi26@gmail.com>
2026-02-16 14:58:53 +0530

    Refactor card hover functionality to manage hover state and highlight the card with the highest z-index

--- Scripts/CardManager/cards_manager.gd
+++ Scripts/CardManager/cards_manager.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var card_being_dragged: Node2D
 var screen_size: Vector2
+var is_hovering_on_card: bool
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -24,7 +25,8 @@ func raycast_check_for_card() -> Node2D:
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
 		#print(result[0].collider.get_parent())
-		return result[0].collider.get_parent()
+		#return result[0].collider.get_parent()
+		return get_card_with_highest_z_index(result)
 	return null
 
 func _process(_delta: float) -> void:
@@ -37,10 +39,17 @@ func connect_card_signals(card: Node2D) -> void:
 	card.connect("hovered_off",_on_card_hovered_off)
 
 func _on_card_hovered_on(card: Node2D) -> void:
-	highlight_card(card,true)
+	if not is_hovering_on_card:
+		highlight_card(card,true)
+		is_hovering_on_card = true
 
 func _on_card_hovered_off(card: Node2D) -> void:
 	highlight_card(card,false)
+	var new_card_hovered = raycast_check_for_card()
+	if new_card_hovered:
+		highlight_card(new_card_hovered,true)
+	else:
+		is_hovering_on_card = false
 
 func highlight_card(card:Node2D,hovered:bool):
 	if hovered:
@@ -49,3 +58,21 @@ func highlight_card(card:Node2D,hovered:bool):
 	else:
 		card.scale = Vector2(1.,1.)
 		card.z_index = 1
+
+func get_card_with_highest_z_index(cards:Array) -> Node2D:
+	var highest_z_card = cards[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+	
+	for i in range(1,cards.size()):
+		var cur_card = cards[i].collider.get_parent()
+		if cur_card.z_index > highest_z_index:
+			highest_z_card = cur_card
+			highest_z_index = cur_card.z_index
+	
+	return highest_z_card
+
+func start_drag() -> void:
+	pass
+
+func finish_drag() -> void:
+	pass
