ecbf98e56e517322ba9605e22a3ab8086c3ffa9a
raviwhitehat <ekravi26@gmail.com>
2026-02-16 13:10:11 +0530

    Add game scene and implement card management functionality

--- Scripts/CardManager/cards_manager.gd
+++ Scripts/CardManager/cards_manager.gd
@@ -4,5 +4,18 @@ func _input(event) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.is_pressed():
 			print("Left")
+			raycast_check_for_card()
 		else:
 			print("Left Released")
+
+func raycast_check_for_card() -> Node2D:
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = 1
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
+		#print(result[0].collider.get_parent())
+		return result[0].collider.get_parent()
+	return null
