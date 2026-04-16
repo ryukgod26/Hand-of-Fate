5c1d43c2e80d3954d2f38ea45ed61efade6e9f64
raviwhitehat <ekravi26@gmail.com>
2026-04-17 01:28:13 +0530

    Enhance enemy turn logic; implement max damage card play and update enemy card attributes

--- Scripts/BattleManager/battle_manager.gd
+++ Scripts/BattleManager/battle_manager.gd
@@ -26,7 +26,12 @@ func enemy_turn() -> void:
 	if empty_monster_card_slots.size() == 0:
 		end_enemy_turn()
 		return
-		
+
+	await try_max_dmg_play_card()
+	
+	end_enemy_turn()
+
+func try_max_dmg_play_card() -> void:
 	var enemy_hand = %EnemyHand.enemy_hand
 	if enemy_hand.size() == 0:
 		end_enemy_turn()
@@ -35,7 +40,7 @@ func enemy_turn() -> void:
 	
 	var max_attack_card = enemy_hand[0]
 	for card in enemy_hand:
-		if card.attack > max_attack_card:
+		if card.attack > max_attack_card.attack:
 			max_attack_card = card
 	
 	var tween = create_tween()
@@ -43,9 +48,12 @@ func enemy_turn() -> void:
 	tween.tween_property(max_attack_card,"global_position",random_empty_monster_card_slot.global_position,CARD_MOVE_SPEED)
 	tween.tween_property(max_attack_card,"scale",Vector2(SMALL_CARD_SCALE,SMALL_CARD_SCALE),CARD_MOVE_SPEED)
 	max_attack_card.play_flip_animation()
-
-	end_enemy_turn()
+	
+	%EnemyHand.remove_card_from_hand(max_attack_card)
+	await get_tree().create_timer(1.0).timeout
+	
 
 func end_enemy_turn() -> void:
+	%Deck.reset_draw()
 	end_turn_btn.visible = true
 	end_turn_btn.disabled = false
--- Scripts/Card/enemy_card.gd
+++ Scripts/Card/enemy_card.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var starting_position
 var card_type
+var attack
 
 func play_flip_animation():
 	$AnimationPlayer.play("card_flip")
